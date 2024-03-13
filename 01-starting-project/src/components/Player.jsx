3e049ea78d62e056bf39bcb00e9b7e52af0b8e11
rapakusandeep <rapakusandeep@gmail.com>
2024-03-14 00:47:12 +0530

    completed_app

--- 01-starting-project/src/components/Player.jsx
+++ 01-starting-project/src/components/Player.jsx
@@ -1,10 +1,31 @@
+import { useState,useRef } from "react";
+
+// by using refs, we can remove the unneccessary states(commented in the below code), and also connect ref to the jsx elements to store its value in two way binding.
 export default function Player() {
+const playerName = useRef();
+
+const [ enteredPlayerName, setEnteredPlayerName ] = useState('');
+//const [ submitted, setSubmitted ] = useState(false);
+
+// function handlePlayerName(event) {
+//   setSubmitted(false);
+//   setEnteredPlayerName(event.target.value);
+// }
+
+function handleClick() {
+  setEnteredPlayerName(playerName.current.value);
+  playerName.current.value = '';
+}
+// in the below code we can see the ternary exp for if condition in h2 tag
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      {/* <h2>Welcome {enteredPlayerName ? enteredPlayerName : 'unknown entity'}</h2> */}
+      
+      <h2>Welcome {enteredPlayerName ?? 'unknown entity'}</h2> 
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        {/* <input type="text" onChange={handlePlayerName} value={enteredPlayerName} /> */}
+        <input type="text" ref={playerName} />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
---
+++ 01-starting-project/src/components/TimerChallenge.jsx
@@ -0,0 +1,71 @@
+import { useState,useRef } from "react";
+import ResultModal from "./ResultModal";
+
+export default function TimerChallenge({ title, targetTime }) {
+const timer = useRef();
+const dialog = useRef();
+
+    // const [ timerStarted,setTimerStarted ] = useState(false);
+    // const [ timerExpired, setTimerExpired ] = useState(false);
+
+    const [ timeRemaining,setTimeRemaining ] = useState(targetTime * 1000);
+    const timerIsActive = timeRemaining > 0 && timeRemaining < targetTime * 1000;
+    
+    if(timeRemaining <= 0) {
+        clearInterval(timer.current);    
+        dialog.current.open();
+    }
+
+    function handleReset() {
+        setTimeRemaining(targetTime * 1000);
+    }
+
+    function handleStart() {
+        timer.current = setInterval(() => {
+            setTimeRemaining(prevTimeRemaining => prevTimeRemaining - 10);
+        }, 10);
+        
+    }
+
+    function handleStop() {
+        dialog.current.open();
+        clearInterval(timer.current);
+    }
+
+    // function handleStart() {
+    //     timer.current = setTimeout(() => {
+    //         setTimerExpired(true);
+    //         dialog.current.open(); //this is the inbuilt method for dialog to be visible
+    //     }, targetTime * 1000);
+    //     setTimerStarted(true);
+    // }
+
+    // function handleStop() {
+    //     clearTimeout(timer.current);
+    // }
+    
+
+    return (
+        <>
+        <ResultModal 
+        ref={dialog} 
+        targetTime={targetTime} 
+        remainingTime={timeRemaining} 
+        onReset={handleReset} />
+        <section className="challenge">
+            <h2>{title}</h2>            
+            <p className="challenge-time">
+                {targetTime} second{targetTime > 1 ? 's' : ''}
+            </p>
+            <p>
+                <button onClick={timerIsActive ? handleStop : handleStart}>
+                    {timerIsActive ? 'Stop' : 'Start' } Challenge
+                </button>
+            </p>
+            <p className={timerIsActive ? 'active' : undefined}>
+                {timerIsActive ? 'Timer is running' : 'Timer inactive'}
+            </p>
+        </section>
+        </>
+    );
+}
